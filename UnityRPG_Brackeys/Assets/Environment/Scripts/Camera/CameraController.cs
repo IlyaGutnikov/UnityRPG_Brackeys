6a89ee64a1147bcd167a65ac2c11795469c57f8e
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2017-07-29 18:09:13 +0300

    Закончил видео: "MOVEMENT - Making an RPG in Unity (E01) "

---
+++ UnityRPG_Brackeys/Assets/Environment/Scripts/Camera/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform TargetToFollow;
+
+    public float Pitch = 2f;
+    public float ZoomSpeed = 4f;
+    public float MinZoom = 5f;
+    public float MaxZoom = 15f;
+
+    public float YawSpeed = 100f;
+
+    public Vector3 Offset;
+
+    private float _currentZoom = 10f;
+    private float _currentYaw = 0f;
+
+    void Update()
+    {
+        _currentZoom -= Input.GetAxis("Mouse ScrollWheel") * ZoomSpeed;
+        _currentZoom = Mathf.Clamp(_currentZoom, MinZoom, MaxZoom);
+
+        _currentYaw -= Input.GetAxis("Horizontal") * YawSpeed * Time.deltaTime;
+    }
+
+    void LateUpdate()
+    {
+
+        transform.position = TargetToFollow.position - Offset * _currentZoom;
+        transform.LookAt(TargetToFollow.position + Vector3.up * Pitch);
+
+        transform.RotateAround(TargetToFollow.position, Vector3.up, _currentYaw);
+    }
+}
--- UnityRPG_Brackeys/Assets/Environment/Scripts/Player/PlayerController.cs
+++ UnityRPG_Brackeys/Assets/Environment/Scripts/Player/PlayerController.cs
@@ -13,6 +13,9 @@ public class PlayerController : MonoBehaviour
     private Camera _cam;
     private PlayerMotor _motor;
 
+    private const int LeftMouseButton = 0;
+    private const int RightMouseButton = 1;
+
 	// Use this for initialization
 	void Start () {
 		_cam = Camera.main;
@@ -20,20 +23,46 @@ public class PlayerController : MonoBehaviour
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
+	    WalkToPoint();
 
-	    if (Input.GetMouseButtonDown(0))
-	    {
-	        Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
-	        RaycastHit hit;
+	    InteractWithObject();
+	}
 
-	        if (Physics.Raycast(ray, out hit, rangeOfHit, MovementMask))
-	        {
+    /// <summary>
+    /// Interact with object (right click)
+    /// </summary>
+    private void InteractWithObject()
+    {
+        if (Input.GetMouseButtonDown(RightMouseButton))
+        {
+            Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, rangeOfHit))
+            {
+                //Check if interactable
+            }
+        }
+    }
+
+    /// <summary>
+    /// Walk to the point (left click)
+    /// </summary>
+    private void WalkToPoint()
+    {
+        if (Input.GetMouseButtonDown(LeftMouseButton))
+        {
+            Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, rangeOfHit, MovementMask))
+            {
                 _motor.MoveToPoint(hit.point);
 
                 //Stop focus
-	        }
-
-	    }
-	}
+            }
+        }
+    }
 }
