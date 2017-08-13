49c4e876d4fa292a6aaeb33dc1bcb5ee7b2e2d77
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2017-08-13 16:09:35 +0300

    Урок 3 - Интеграция анимации

---
+++ UnityRPG_Brackeys/Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class CharacterAnimator : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+    private Animator _animator;
+
+    private float _speed;
+
+    private const float Smooth = .1f;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _agent = GetComponent<NavMeshAgent>();
+	    _animator = GetComponentInChildren<Animator>();
+
+	    if (_animator == null)
+	    {
+            Debug.LogError("Animator is required");
+	    }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    _speed = _agent.velocity.magnitude / _agent.speed;
+        _animator.SetFloat("speedPercentage", _speed, Smooth, Time.deltaTime);
+	}
+}
