1d831a3e907c1ce6256c2e438a26fa3e65f08b3e
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2018-01-06 18:14:09 +0300

    E9

---
+++ UnityRPG_Brackeys/Assets/Scripts/Stats/CharacterStats.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    public int MaxHealth = 100;
+    public int CurrentHealth { get; private set; }
+
+    public Stat Damage;
+    public Stat Armor;
+
+    void Awake()
+    {
+        CurrentHealth = MaxHealth;
+    }
+
+#if DEVELOPMENT_BUILD || UNITY_EDITOR
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            TakeDamage(10);
+        }
+    }
+#endif
+
+    public void TakeDamage(int damage)
+    {
+        damage -= Armor.GetValue();
+        damage = Mathf.Clamp(damage, 0, int.MaxValue);
+
+        CurrentHealth -= damage;
+        Debug.Log(transform.name + " takes damage: " + damage);
+
+        if (CurrentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public virtual void Die()
+    {
+        Debug.Log(transform.name + " died.");
+    }
+}
---
+++ UnityRPG_Brackeys/Assets/Scripts/Stats/PlayerStats.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : CharacterStats {
+
+    void Start()
+    {
+        EquipmentManager.Instance.OnEquipmentChanged += OnEquipmentChanged;
+    }
+
+    private void OnEquipmentChanged(Equipment newItem, Equipment oldItem)
+    {
+        if (newItem != null)
+        {
+            Armor.AddModifier(newItem.ArmorModifier);
+            Damage.AddModifier(newItem.DamageModifier);
+        }
+
+        if (oldItem != null)
+        {
+            Armor.RemoveModifier(oldItem.ArmorModifier);
+            Damage.RemoveModifier(oldItem.DamageModifier);
+        }
+    }
+
+    public override void Die()
+    {
+        base.Die();
+    }
+}
